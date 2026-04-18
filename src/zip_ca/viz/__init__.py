eca67ee1e0d0fe2dbbdb6e7685964497221a53cf
Gabriel Mitelman Tkacz <gabriel@tkacz.dev.br>
2026-04-17 21:43:30 -0300

    refactor: repository reset

--- src/zip_ca/viz/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Visualisation subpackage for the Zip cellular-automata solver."""
-
-from .panel_a import render_path_layer
-from .panel_b import render_chem_layer
-
-__all__ = ["render_chem_layer", "render_path_layer"]
